fe6453beedccaf1fdca9c1d0e696b2718221243b
FishHawk <whang177@gmail.com>
2021-11-16 15:16:55 +0800

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,22 +15,24 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.3")
 
-    implementation("io.ktor:ktor-locations:1.6.4")
-    implementation("io.ktor:ktor-serialization:1.6.4")
-    implementation("io.ktor:ktor-server-core:1.6.4")
-    implementation("io.ktor:ktor-server-netty:1.6.4")
-    implementation("io.ktor:ktor-client-core:1.6.4")
-    implementation("io.ktor:ktor-client-cio:1.6.4")
-    implementation("io.ktor:ktor-client-serialization:1.6.4")
-    testImplementation("io.ktor:ktor-server-test-host:1.6.4")
+    val ktorVersion = "1.6.5"
+    implementation("io.ktor:ktor-locations:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
 
     implementation("org.codehaus.janino:janino:3.1.6")
     implementation("ch.qos.logback:logback-classic:1.2.6")
 
     implementation("se.sawano.java:alphanumeric-comparator:1.4.1")
 
-    testImplementation("io.kotest:kotest-runner-junit5:4.6.3")
-    testImplementation("io.kotest:kotest-assertions-core:4.6.3")
+    val kotestVersion = "4.6.3"
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 application {
